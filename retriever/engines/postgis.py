bd2b413ee813450f4ec0e3a712415f464439207d
Kunal Pal <mail.kunalpal@gmail.com>
2017-03-08 19:36:56 +0100

    Inheriting postgis engine from postgres engine

--- retriever/engines/postgis.py
+++ retriever/engines/postgis.py
@@ -1,10 +1,12 @@
-import os
 from __future__ import print_function
-from retriever.lib.models import Engine, no_cleanup
+
+import os
+from retriever.engines.postgres import engine as PostgreSQLEngine
+from retriever.lib.models import no_cleanup
 from retriever import ENCODING
 
 
-class engine(Engine):
+class engine(PostgreSQLEngine):
     """Engine instance for PostgreSQL."""
     name = "PostGIS"
     abbreviation = "postgis"
@@ -43,7 +45,7 @@ class engine(Engine):
 
     def create_db_statement(self):
         """In PostgreSQL, the equivalent of a SQL database is a schema."""
-        db_statement = Engine.create_db_statement(self).replace("DATABASE", "SCHEMA")
+        db_statement = PostgreSQLEngine.create_db_statement(self)
         if not hasattr(self.script, "spatial"):
             print("Tabular data. Falling back to PostgreSQL engine.\n")
             return db_statement
@@ -57,103 +59,44 @@ class engine(Engine):
                         + ["CREATE EXTENSION IF NOT EXISTS {};".format(extension)
                            for extension in ["postgis", "postgis_sfcgal", "postgis_topology"]])
 
-    def create_db(self):
-        """Creates the database"""
-        try:
-            Engine.create_db(self)
-        except:
-            self.connection.rollback()
-            pass
-
     def create_table(self):
         """PostgreSQL needs to commit operations individually."""
-        Engine.create_table(self)
-        self.connection.commit()
-
-    def drop_statement(self, objecttype, objectname):
-        """In PostgreSQL, the equivalent of a SQL database is a schema."""
-        statement = Engine.drop_statement(self, objecttype, objectname)
-        statement += " CASCADE;"
-        return statement.replace(" DATABASE ", " SCHEMA ")
+        return PostgreSQLEngine.create_table(self)
 
     def escape_single_quotes(self, value):
         """Escapes single quotes in the value"""
-        return value.replace("'", "''")
+        return PostgreSQLEngine.escape_single_quotes(self, value)
 
     def insert_data_from_file(self, filename):
         """Use PostgreSQL's "COPY FROM" statement to perform a bulk insert."""
-        self.get_cursor()
-        ct = len([True for c in self.table.columns if c[1][0][:3] == "ct-"]) != 0
-        if (([self.table.cleanup.function, self.table.delimiter,
-              self.table.header_rows] == [no_cleanup, ",", 1])
-            and not self.table.fixed_width
-            and not ct
-            and (not hasattr(self.table, "do_not_bulk_insert") or not self.table.do_not_bulk_insert)):
-            columns = self.table.get_insert_columns()
-            filename = os.path.abspath(filename)
-            statement = """
-COPY """ + self.table_name() + " (" + columns + """)
-FROM '""" + filename.replace("\\", "\\\\") + """'
-WITH DELIMITER ','
-CSV HEADER;"""
-            try:
-                self.execute("BEGIN")
-                self.execute(statement)
-                self.execute("COMMIT")
-            except:
-                self.connection.rollback()
-                return Engine.insert_data_from_file(self, filename)
-        else:
-            return Engine.insert_data_from_file(self, filename)
-
-    def insert_statement(self, values):
-        """Returns a SQL statement to insert a set of values"""
-        statement = Engine.insert_statement(self, values)
-        if isinstance(statement, bytes):
-            statement = statement.decode("utf-8", "ignore")
-        return statement
-
-    def table_exists(self, dbname, tablename):
-        """Checks to see if the given table exists"""
-        if not hasattr(self, 'existing_table_names'):
-            self.cursor.execute(
-                "SELECT schemaname, tablename FROM pg_tables WHERE schemaname NOT LIKE 'pg_%';")
-            self.existing_table_names = set()
-            for schema, table in self.cursor:
-                self.existing_table_names.add((schema.lower(), table.lower()))
-        return (dbname.lower(), tablename.lower()) in self.existing_table_names
+#         self.get_cursor()
+#         ct = len([True for c in self.table.columns if c[1][0][:3] == "ct-"]) != 0
+#         if (([self.table.cleanup.function, self.table.delimiter,
+#               self.table.header_rows] == [no_cleanup, ",", 1])
+#             and not self.table.fixed_width
+#             and not ct
+#             and (not hasattr(self.table, "do_not_bulk_insert") or not self.table.do_not_bulk_insert)):
+#             columns = self.table.get_insert_columns()
+#             filename = os.path.abspath(filename)
+#             statement = """
+# COPY """ + self.table_name() + " (" + columns + """)
+# FROM '""" + filename.replace("\\", "\\\\") + """'
+# WITH DELIMITER ','
+# CSV HEADER;"""
+#             try:
+#                 self.execute("BEGIN")
+#                 self.execute(statement)
+#                 self.execute("COMMIT")
+#             except:
+#                 self.connection.rollback()
+#                 return Engine.insert_data_from_file(self, filename)
+#         else:
+#             return Engine.insert_data_from_file(self, filename)
+        return PostgreSQLEngine.insert_data_from_file(self, filename)
 
     def format_insert_value(self, value, datatype):
         """Formats a value for an insert statement"""
-        if datatype == "bool":
-            try:
-                if int(value) == 1:
-                    return "TRUE"
-                elif int(value) == 0:
-                    return "FALSE"
-            except:
-                pass
-        return Engine.format_insert_value(self, value, datatype)
-
-    def get_connection(self):
-        """Gets the db connection.
-           
-           Please update the encoding lookup table if the required encoding is not present.
-        """
-        import psycopg2 as dbapi
-        self.get_input()
-        conn = dbapi.connect(host=self.opts["host"],
-                             port=int(self.opts["port"]),
-                             user=self.opts["user"],
-                             password=self.opts["password"],
-                             database=self.opts["database"])
-        encoding = ENCODING.lower()
-        if self.script.encoding:
-            encoding = self.script.encoding.lower()
-        encoding_lookup = {'iso-8859-1': 'Latin1', 'latin-1': 'Latin1', 'utf-8': 'UTF8'}
-        db_encoding = encoding_lookup.get(encoding)
-        conn.set_client_encoding(db_encoding)
-        return conn
+        return PostgreSQLEngine.format_insert_value(self, value, datatype)
 
     def check_postgis_availability(self):
         self.cursor.execute("SELECT PostGIS_Version();")
