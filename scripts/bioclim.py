00733c0b15fd06e10c448093bb707b4b8c109f10
Kunal Pal <mail.kunalpal@gmail.com>
2017-03-08 22:24:29 +0100

    Modifying bioclim dataset for proper installation

--- scripts/bioclim.py
+++ scripts/bioclim.py
@@ -1,9 +1,10 @@
-#retriever
+# retriever
 
 """Retriever script for direct download of Bioclim data"""
 from builtins import range
 
 from retriever.lib.templates import Script
+from retriever.lib.models import Table, Cleanup
 
 
 class main(Script):
@@ -18,17 +19,36 @@ class main(Script):
         self.retriever_minimum_version = '2.0.dev'
         self.version = '1.1.0'
         self.tags = ["climate"]
-        
-    def download(self, engine=None, debug=False):
-        if engine.name != "Download Only":
-            raise Exception("The Bioclim dataset contains only non-tabular data files, and can only be used with the 'download only' engine.")
-        Script.download(self, engine, debug)
+        self.spatial = "raster"
+
+    def download(self, engine=None, debug=False, use_cache=True):
+        if engine.name not in ["Download Only", "PostGIS"]:
+            raise Exception("The Bioclim dataset contains only non-tabular data files.\n "
+                            "It can only be used with the 'download only' or 'postgis' engine.")
+        _engine = engine
+        Script.download(self, engine, debug, use_cache)
         file_names = []
         for file_num in range(1, 20):
             for ext in (['bil', 'hdr']):
                 file_names += ["bio{0}.{1}".format(file_num, ext)]
-        self.engine.download_files_from_archive(self.urls["climate"], file_names)
-        self.engine.register_files(file_names)
+        _engine.download_files_from_archive(self.urls["climate"], file_names)
+        if _engine.name is "Download Only":
+            _engine.register_files(file_names)
+        elif _engine.name is "PostGIS":
+            hdr_files = [file_name[:-4] for file_name in file_names
+                         if file_name[-3:] == "hdr"]
+            self.tables = {}
+            for hdr_file in hdr_files:
+                with open(_engine.format_filename(hdr_file + ".hdr"), "r") as hdr_content:
+                    table = {}
+                    for line in hdr_content:
+                        if "Variable" in line:
+                            table["name"] = line.split("Variable")[-1].split("=")[0].strip()
+                    hdr_content.close()
+                    if "name" in table.keys():
+                        _engine.insert_data_from_file(_engine.format_filename(hdr_file + ".bil"),
+                                                      table_name=table["name"])
+
 
 SCRIPT = main()
 
